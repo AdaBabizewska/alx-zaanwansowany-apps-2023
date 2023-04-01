de8f1c4269285e35f585407443faabea6b5a181e
Adrianna Babiżewska <a.babizewska@gmail.com>
2023-04-01 16:25:59 +0200

    wersja druga

---
+++ 00-prework-js/zadanie.js
@@ -0,0 +1,31 @@
+
+const products = [
+    {
+      name: 'banan',
+      price: 4.99
+    },
+    {
+      name: "chleb",
+      price: 3.25
+    },
+    {
+      name: 'ser',
+      price: 7.00
+    },
+    {
+      name: 'baton',
+      price: 1.99
+    }
+  ]
+
+  
+  let sumaZakupy = 0;
+
+  for (let y of products) {
+    sumaZakupy = sumaZakupy + y.price
+  }
+
+  let liczbaProduktow = products.length
+  let sredniaZakupow = sumaZakupy/liczbaProduktow
+
+  console.log (sumaZakupy, liczbaProduktow, sredniaZakupow)
